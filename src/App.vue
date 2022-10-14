0a5f847ef1de7c40eb17af13a4b16505de128007
Mateusz <kroplewskimateusz@gmail.com>
2022-10-14 10:57:35 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Nav></Nav>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+
+export default{
+  components:{
+    Nav,
+  },
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="w-[100vw] h-[70px] bg-[#222222] text-purple-400 flex">
+        <div class="flex ml-10 hover:cursor-pointer mt-[15px]">
+            <img src="@/assets/logo.png" alt="" class="w-[40px] h-[40px]">
+            <p class="font-bold text-[20px] ml-[5px]">Blogger</p>
+        </div>
+        <div class="flex mx-auto mt-[20px]">
+            <input type="text" name="" id="" placeholder="Search for Blogs" class="w-[300px] h-[30px] rounded-md bg-[#333333] font-semibold pl-[5px] mr-10">
+            <a href="#" class="font-bold ">Write Blog</a>
+        </div>
+        <div class="float-right hover:cursor-pointer">
+            <img src="@/assets/user.png" alt="" class="w-[50px] h-[50px] rounded-full bg-[#333333] mr-5 mt-[7px]">
+        </div>
+    </nav>
+</template>
