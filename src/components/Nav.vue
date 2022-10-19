d648455a6d3ebdcf4c4a017cee852a8e7e7ed4c2
Mateusz <kroplewskimateusz@gmail.com>
2022-10-19 14:40:16 +0100

    fixed css bug (markdown not working)

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,11 +2,11 @@
     <nav class="w-[100vw] h-[70px] bg-[#222222] text-purple-400 flex relative">
         <div class="flex md:ml-10 ml-5 hover:cursor-pointer mt-[15px]">
             <img src="@/assets/logo.png" alt="" class="w-[40px] h-[40px]">
-            <p class="font-bold text-[20px] ml-[5px]">Blogger</p>
+            <p class="font-bold text-[20px] ml-[10px] mt-[3px]">Blogger</p>
         </div>
         <div class="flex mx-auto mt-[20px] hidden md:inline">
             <input type="text" name="" id="" placeholder="Search for Blogs" class="w-[300px] h-[30px] rounded-md bg-[#333333] font-semibold pl-[5px] mr-10">
-            <a href="#" class="font-bold ">Write Blog</a>
+            <a href="#" class="font-bold text-purple-400 no-underline">Write Blog</a>
         </div>
         <div class="float-right hover:cursor-pointer hidden md:inline">
             <img src="@/assets/user.png" alt="" class="w-[50px] h-[50px] rounded-full bg-[#333333] mr-5 mt-[7px]">
--- src/components/createBlog.vue
+++ src/components/createBlog.vue
@@ -1,28 +1,34 @@
+
 <template>
+    <!-- Edit blog -->
     <div class="max-w-[1000px] h-[500px] mx-auto mt-16 text-center flex flex-col">
         <button class="w-[150px] min-h-[40px] rounded-lg bg-purple-500 font-bold mb-10 hover:bg-purple-700">Add Cover Img</button>
         <input type="text" name="title" placeholder="Title" v-model="title"
-         class="bg-[#222222] focus:outline-none text-[#EEEEEE] w-[80%] h-[50px] pl-[10px] font-bold text-[30px] rounded-md">
+         class="bg-[#222222] focus:outline-none text-[#EEEEEE] w-[80%] min-h-[50px] pl-[10px] font-bold text-[30px] rounded-md">
         <hr class="mt-10 border-[1px] border-solid border-[#333333]">
-        <div class="flex pt-[10px]">
-            <button class="w-[70px] h-[30px] rounded-lg font-semibold text-purple-300 hover:bg-gray-200 hover:text-[#222222] text-[20px]">Write</button>
-            <button class="w-[90px] h-[30px] rounded-lg font-semibold text-gray-300 hover:bg-gray-200 hover:text-[#222222] text-[20px] ml-5">Preview</button>
-            <div class="flex ml-[30px] mt-[3px] text-gray-300 space-x-4">
+        <div class="flex pt-[10px] mx-auto">
+            <button @click.prevent="preview = false" class="w-[70px] h-[30px] rounded-lg font-semibold text-purple-300 hover:bg-gray-200 hover:text-[#222222] text-[20px]">Write</button>
+            <button @click.prevent="preview = true" class="w-[90px] h-[30px] rounded-lg font-semibold text-gray-300 hover:bg-gray-200 hover:text-[#222222] text-[20px] ml-5">Preview</button>
+        </div>
+        <hr class="mt-[15px] border-[1px] border-solid border-[#333333]">
+        <div v-if="!preview" class="w-[100%]">
+            <div class="flex  mt-5 text-gray-300 space-x-4">
                 <p class="p-0 m-0 font-bold">Key:</p>
                 <p class="p-0 m-0 bg-[#222222] px-2 rounded-lg"># : h1</p>
                 <p class="p-0 m-0 bg-[#222222] px-2 rounded-lg">*example* : italic</p>
                 <p class="p-0 m-0 bg-[#222222] px-2 rounded-lg">**example** : bold</p>
                 <p class="p-0 m-0 bg-[#222222] px-2 rounded-lg">- example : unordered list</p>
                 <p class="p-0 m-0 bg-[#222222] px-2 rounded-lg">1. : ordered list</p>
-
             </div>
+            <textarea name="content" placeholder="Your story starts here..." v-model="content"
+            class="bg-[#222222] rounded-lg mt-10 min-h-[300px] w-[1000px] focus:outline-none text-gray-200 pl-[10px] pt-[10px] font-semibold"></textarea>
         </div>
-        <hr class="mt-[15px] border-[1px] border-solid border-[#333333]">
-        <textarea name="content" placeholder="Your story starts here..." v-model="content"
-        class="bg-[#222222] rounded-lg mt-10 min-h-[300px] focus:outline-none text-gray-200 pl-[10px] pt-[10px] font-semibold"></textarea>
+        <!-- Preview Blog -->
+        <div v-else class="text-left">
+            <div class="text-gray-100" v-html="markdown"></div> 
+        </div>
 
     </div>
-    <div class="text-gray-100" v-html="markdown"></div> 
 </template>
 
 <script>
@@ -31,7 +37,7 @@ import {marked} from 'marked';
 
     let title = ref('');
     let content = ref('');
-
+    let preview = ref(false);
     export default{
         setup(){
 
@@ -43,7 +49,8 @@ import {marked} from 'marked';
             return{
                 title,
                 content,
-                markdown
+                markdown,
+                preview
             }
     }
 }
@@ -51,7 +58,7 @@ import {marked} from 'marked';
 
 <style>
 p{
-    all:revert;
+    all: revert;
 }
 h1{
     all:revert;
