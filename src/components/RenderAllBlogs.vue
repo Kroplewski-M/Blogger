f4d66bfbdd7867f77bfcc8f22c31dc82dc7d0414
Mateusz <kroplewskimateusz@gmail.com>
2022-10-23 19:22:55 +0100

    basic blog card design added

--- src/components/RenderAllBlogs.vue
+++ src/components/RenderAllBlogs.vue
@@ -1,3 +1,60 @@
 <template>
-    
+    <section class="max-w-[1000px] h-[100vh] mx-auto mt-16 flex space-x-10 flex-wrap">
+        <div v-for="blog in blogs">
+            <div class="w-[300px] h-[400px] bg-[#222222] rounded-md relative overflow-hidden hover:cursor-pointer">
+                <div class="w-[300px]">
+                    <img src='@/assets/blog-standard.jpg' alt="">
+                </div>
+                <div class="flex mt-[5px] ml-[5px]">
+                    <img src="@/assets/comments.png" alt="" class="w-[20px] h-[20px] mr-[5px]">
+                    <p class="text-gray-500 mr-5 -mt-[1px]">5</p>
+                    <img src="@/assets/like.png" alt="" class="w-[20px] h-[20px] mr-[5px]">
+                    <p class="text-gray-500 -mt-[1px]">12</p>
+                </div>
+                <h1 class="text-center -mt-[5px] text-gray-200">{{blog.title}}</h1>
+                <p class="text-center text-gray-500 -mt-[10px]">{{blog.overView}}</p>
+
+                <div class="flex">
+                    <img src="@/assets/user.png" alt="" class="w-[50px] h-[50px]">
+                    <p class="text-gray-300 ml-[5px] font-semibold">Mateusz Kroplwski</p>
+                </div>
+                <p class="-mt-[4px] ml-[5px] text-gray-500">{{blog.date}}</p>
+            </div>
+        </div>
+    </section>
 </template>
+
+<script>
+import {ref} from 'vue';
+
+    export default{
+        setup(){
+
+            let blogs = ref({
+            0:{title: "Begginer JS", 
+            date: "Sun Jun 14 2020",
+            overView: "This is a begginer guide to JavaScript!",
+            content: "",
+            imgRef: "blog-Standard.png",
+            userID: "",},
+
+            1:{title: "Advanced JS", 
+            date: "Mon Jun 15 2020",
+            overView: "This is a advanced guide to JavaScript!",
+            content: "",
+            imgRef: "blog-Standard.png",
+            userID: "",}
+
+            });
+            console.log(blogs._rawValue);
+            return{
+                blogs
+            }
+        }
+    }
+</script>
+<style>
+* {
+	overflow-x: hidden;
+}
+</style>
