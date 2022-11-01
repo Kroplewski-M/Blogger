c029e02d904842881154141d28860963a2d03062
Mateusz <kroplewskimateusz@gmail.com>
2022-11-01 03:03:29 +0000

    register form validated

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,17 +6,17 @@
     <div class="w-[300px] md:w-[500px] h-[500px] rounded-md bg-[#222222] mx-auto mt-[100px] text-purple-300 font-semibold">
         <h1 class="text-purple-400 font-bold text-center text-[40px] mt-[5px]">Register</h1>
         <div class="w-[250px] md:w-[370px] mx-auto mt-[55px]">
-            <vee-form action="" class="md:text-[20px] text-[15px]" :validation-schema="schema" >
+            <vee-form action="" class="md:text-[20px] text-[15px]" :validation-schema="schema" @submit="registerAccount">
                 <label for="name" class="ml-[35px]">Name:</label>
-                <vee-field type="text" name="name" placeholder="John Doe" class="w-[150px] md:w-[250px] h-[30px] rounded-sm bg-[#333333] pl-[5px] ml-[7px] mb-[30px]" />
+                <vee-field type="text" name="name" placeholder="John Doe" class="w-[150px] md:w-[250px] h-[30px] rounded-sm bg-[#333333] pl-[5px] ml-[7px] mb-[30px] text-purple-500" />
                 <ErrorMessage name="name"  class="text-red-500 block text-[13px] -mt-[30px]  ml-[100px] absolute"/>
 
                 <label for="email" class="ml-[35px]">Email:</label>
-                <vee-field type="email" name="email" placeholder="example@mail.com" class="w-[150px] md:w-[250px] h-[30px] rounded-sm bg-[#333333] pl-[5px] ml-[10px] mb-[30px]" />
+                <vee-field type="email" name="email" placeholder="example@mail.com" class="w-[150px] md:w-[250px] h-[30px] rounded-sm bg-[#333333] pl-[5px] ml-[10px] mb-[30px] text-purple-500" />
                 <ErrorMessage name="email"  class="text-red-500 w-[100%] block text-[13px] absolute -mt-[30px]  ml-[100px]"/>
 
                 <label for="password">Password:</label>
-                <vee-field type="password" name="password" placeholder="password" class="w-[150px] md:w-[250px] h-[30px] rounded-sm bg-[#333333] pl-[5px] md:ml-[10px] ml-[17px]" />
+                <vee-field type="password" name="password" placeholder="password" class="w-[150px] md:w-[250px] h-[30px] rounded-sm bg-[#333333] pl-[5px] md:ml-[10px] ml-[17px] text-purple-500" />
                 <ErrorMessage name="password"  class="text-red-500 w-[100%] block text-[13px] absolute  ml-[100px]"/>
 
                 <div class="w-[110px] mx-auto mt-[50px]">
@@ -38,17 +38,18 @@ import {supabase} from '../includes/supabase';
 export default{
     setup(){
         let loading = ref(false);
-        let email = ref('');
-        let name = ref('');
-        let password = ref('');
         let schema= ref({
-            name: "required",
-            email: "required",
-            password: "required",
+            name: "required|alpha_spaces",
+            email: "required|email",
+            password: "required|min:6",
         });
+        function registerAccount(values){
+            console.log(values);
+        }
 
         return{
-            schema
+            schema,
+            registerAccount
         }
     }
 }
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,8 +3,14 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from "vee-validate";
-import { required } from "@vee-validate/rules";
+import {
+  required,
+  min,
+  alpha_spaces as alphaSpaces,
+  email,
+} from "@vee-validate/rules";
 
 export default {
   install(app) {
@@ -13,5 +19,24 @@ export default {
     app.component("ErrorMessage", ErrorMessage);
 
     defineRule("required", required);
+    defineRule("min", min);
+    defineRule("alpha_spaces", alphaSpaces);
+    defineRule("email", email);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required!`,
+          min: `The field ${ctx.field} is too short!`,
+          alpha_spaces: `The field ${ctx.field} may only contain letters!`,
+          email: `This field must contain a valid email format!`,
+        };
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid!`;
+
+        return message;
+      },
+    });
   },
 };
