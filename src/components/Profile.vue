446b9c85e0936d7c6f49ea631fb548118a50cf23
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-11-07 14:50:08 +0000

    started password rec

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -5,6 +5,11 @@
                 <p class="text-center text-[16px] font-bold my-0 mt-[3px]">Logging Out...</p>
             </div>
         </div>
+        <div v-if="sentPasswordLink" class="w-[300px] mx-auto">
+            <div class="absolute w-[300px] h-[30px] rounded-md bg-green-500 -mt-[20px]">
+                <p class="text-center text-[16px] font-bold my-0 mt-[3px]">{{passwordResetText}}</p>
+            </div>
+        </div>
         <div class="max-w-[500px] mx-auto pt-10">
             <div class="w-[100px] mx-auto">
                 <img :src="profileStore.user.avatarUrl" alt="" class="w-[100px] h-[100px]  bg-gray-300 rounded-full">
@@ -13,12 +18,12 @@
             <div class="mt-10 rounded-md md:pl-5 md:text-[20px]">
                 <p class=" font-semibold text-gray-100 bg-[#333333] py-[10px] px-[10px] rounded-md w-[150px]">Your details:</p>
             <div>
-                <form v-if="updateForm" action="" class="mt-5 font-semibold text-purple-400 w-[200px] md:w-[320px]">
+                <form v-if="updateForm" action="" class="mt-5 font-semibold text-purple-400 w-[200px] md:w-[380px] mx-auto md:mx-0 mb-10">
                     <label for="name" class="">Name:</label>
-                    <input type="text" name="name" id="name" placeholder="Mateusz Kroplewski" class="md:ml-[5px] w-[200px] h-[30px] rounded-sm bg-[#222222] pl-[5px] mb-[25px] text-gray-200" >
+                    <input type="text" name="name" id="name" class="md:ml-[5px] w-[200px] md:w-[280px] h-[30px] rounded-sm bg-[#222222] pl-[5px] mb-[25px] text-gray-200" :value="profileName">
 
-                    <label for="email" class="ml-[5px]">Email:</label>
-                    <input type="email" name="email" id="email" placeholder="mateusz@gmail.com" class="md:ml-[5px] w-[200px] h-[30px] rounded-sm bg-[#222222] pl-[5px] text-gray-200" >
+                    <label for="email" class="">Email:</label>
+                    <input type="email" name="email" id="email" class="md:ml-[5px] w-[300px] h-[30px] rounded-sm bg-[#222222] pl-[5px] text-gray-200" :value="profileEmail">
                 </form>
                 <div v-else class="text-gray-500 font-semibold w-[100%] md:max-w-[auto]">
                     <p class="bg-[#333333] py-[10px] px-[10px] rounded-md">ID: <span class="font-bold text-gray-200 ml-[10px] text-[13px] md:text-[20px]">{{profileStore.user.id}}</span></p>
@@ -26,10 +31,11 @@
                     <p class="bg-[#333333] py-[10px] px-[10px] rounded-md">Email: <span class="font-bold text-gray-200 ml-[10px]">{{profileStore.user.email}}</span></p>
                 </div>
                 <div class="w-[170px] md:w-[auto] mx-auto md:mx-0">
-                    <button class="mt-10 w-[170px] bg-gray-100 rounded-md text-[#111111] mb-10">Reset Password</button>
+                    <button v-if="updateForm == false" @click.prevent="resetPassword" class="mt-10 w-[170px] bg-gray-100 rounded-md text-[#111111] mb-10">Reset Password</button>
                     <div class="md:flex md:space-x-5 mb-5 font-semibold">
-                        <button class="mt-5 w-[170px] bg-purple-500 rounded-md text-[#111111]">Update Info</button>
-                        <button class="mt-5 w-[170px] bg-purple-500 rounded-md text-[#111111]" @click.prevent="logOut">Log out</button>
+                        <button class="mt-5 w-[170px] bg-purple-500 rounded-md text-[#111111]" @click.prevent="updateForm = true" v-text="formBtnText"></button>
+                        <button v-if="updateForm == false" class="mt-5 w-[170px] bg-purple-500 rounded-md text-[#111111]" @click.prevent="logOut">Log out</button>
+                        <button v-if="updateForm == true" class="mt-5 w-[170px] bg-purple-500 rounded-md text-[#111111]" @click.prevent="updateForm = false">Cancel</button>
                     </div>
                 </div>
 
@@ -44,13 +50,14 @@
 import {useProfileStore} from '../stores/profile';
 import {supabase} from '../includes/supabase';
 import { ref } from 'vue';
-
+import { watch } from 'vue';
 
 export default{
     setup(){        
         const profileStore = useProfileStore();
         let updateForm = ref(false);
         let loggingOut = ref(false);
+        let formBtnText = ref('Update Info');
         async function logOut(){
             try{
                 loggingOut.value = true;
@@ -62,13 +69,45 @@ export default{
             }catch{
                 alert(error);
             }
+        };
+        watch(updateForm,() =>{
+            if(updateForm.value == true){
+                formBtnText.value = 'Confirm change';
+            }
+            else{
+                formBtnText.value = 'Update Info';
+            }
+        });
 
+        let sentPasswordLink = ref(false);
+        let passwordResetText = ref('Sending email...');
+        async function resetPassword(){
+            passwordResetText.value = 'Sending email...';
+            sentPasswordLink.value = true;
+            try{
+                const { data, error } = await supabase.auth.resetPasswordForEmail('kroplewskimateusz@gmail.com', {redirectTo: 'http://localhost:5173/'});
+                if(error) throw error;
+                else{ passwordResetText.value = 'Email sent!'}
+            }catch(error){
+                passwordResetText.value = 'Error occured! Please try again shortly...';
+            }
         }
+
+        let profileEmail = ref(profileStore.user.email);
+        let profileName = ref(profileStore.user.name);
+
+
         return{
             profileStore,
             updateForm,
             logOut,
-            loggingOut
+            loggingOut,
+            formBtnText,
+            profileEmail,
+            profileName,
+            resetPassword,
+            sentPasswordLink,
+            passwordResetText
         }
     }
 }
